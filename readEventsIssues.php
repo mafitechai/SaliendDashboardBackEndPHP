f48b01fc8adca1fe19723c7242ccdfee5d5665db
mafitechai <info@mafi.ai>
2023-05-08 18:56:05 -0500

    added endpoint to get weekly issues

---
+++ readEventsIssues.php
@@ -0,0 +1,46 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json; charset=UTF-8');
+
+include_once 'config/database.php';
+include_once 'class/events.php';
+
+$database = new Database();
+$db = $database->getConnection();
+$events = new Events($db);
+
+
+$issues = $events->getWeeklyEvents($_GET['group'], 'issues');
+$disabled = $events->getWeeklyEvents($_GET['group'], 'disabled');
+$failed = $events->getWeeklyEvents($_GET['group'], 'failed');
+// print_r($events->getDailyEvents($_GET['group'], 'failed'));
+$dailyDisabled = $events->getDailyEvents($_GET['group'], 'disabled');
+$dailyIssues = $events->getDailyEvents($_GET['group'], 'issues');
+
+$today = new DateTime(); // today
+$begin = new DateTime(date('Y-m-d', strtotime('monday this week')));
+$end = new DateTime();
+$interval = new DateInterval('P1D'); // 1d interval range
+$daterange = new DatePeriod($begin, $interval, $end); // it always runs forwards in date
+foreach ($daterange as $date) {
+    // date object
+    $d[] = $date->format('Y-m-d'); // your date
+}
+
+
+$dataDisabled = $events->getListIssues($d, $dailyDisabled);
+$dataIssues = $events->getListIssues($d, $dailyIssues);
+$failuresToday = $events->countEvents('today', 'failed');
+
+
+$data['issues'] = $issues['Count'];
+$data['disabled'] = $disabled['Count'];
+$data['failed'] = $failed['Count'];
+$data['graphDisabled'] = array_values($dataDisabled);
+$data['graphIssues'] = array_values($dataIssues);
+
+echo json_encode($data);
+
+
+?>
+
